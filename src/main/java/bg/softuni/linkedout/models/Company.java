2f5152e2fe043a7ceebbbcd27139091bbd4d9ecf
Mihaela <mihaelabataklieva2006@gmail.com>
2024-06-03 18:06:13 +0300

    Adding database entities

---
+++ src/main/java/bg/softuni/linkedout/models/Company.java
@@ -0,0 +1,73 @@
+package bg.softuni.linkedout.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "companies")
+public class Company extends BaseEntity{
+    //budget – a number. (must be a positive number). Cannot be null.
+    //
+    //· description – a very long char sequence. Cannot be null. Must be at least 10 characters.
+    //
+    //· name – a char sequence. Must be unique and cannot be null. Must be between 2 and 10 characters.
+    //
+    //· town – a char sequence. Cannot be null. Must be between 2 and 10 characters.
+    private BigDecimal budget;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String name;
+    private String town;
+    @OneToMany(mappedBy = "company")
+    private List<Employee> employees;
+
+    public Company() {
+        this.employees = new ArrayList<>();
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public BigDecimal getBudget() {
+        return budget;
+    }
+
+    public void setBudget(BigDecimal budget) {
+        this.budget = budget;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
---
+++ src/main/java/bg/softuni/linkedout/models/Employee.java
@@ -0,0 +1,93 @@
+package bg.softuni.linkedout.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+public class Employee extends BaseEntity{
+    //birthdate – a date. Cannot be null.
+    //
+    //· educationLevel – a char sequence. Cannot be null.
+    //
+    //· firstName – a char sequence. Cannot be null. Must be at least 2 characters. Cannot be null.
+    //
+    //· jobTitle – a char sequence. Cannot be null.
+    //
+    //· lastName – a char sequence. Cannot be null. Must be at least 2 characters. Cannot be null.
+    //
+    //· salary – a number (must be a positive number). Cannot be null.
+    private LocalDate birthdate;
+    @Column(name = "education_level")
+    private String educationLevel;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "job_title")
+    private String jobTitle;
+    @Column(name = "last_name")
+    private String lastName;
+    private BigDecimal salary;
+    @ManyToOne
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(String educationLevel) {
+        this.educationLevel = educationLevel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
